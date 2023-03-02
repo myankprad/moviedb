60242d1578591218e6ff94e2ed193c96d8a68b3a
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-03-02 20:59:18 +0530

    commit

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,11 +6,8 @@ const Movie = (props) => {
 
     return (
     <>
-     {props.movies.map((movie, index)=>{
-       <div className='image-container d-flex justify-content-start m-3'>
-       <img src={movie.Poster} ></img>
-       </div>
-     })}
+     
+    
     </>
   )
 }
